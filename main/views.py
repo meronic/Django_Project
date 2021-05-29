f5f8fd60e670866d48889e4b7c4800e3755699c6
meronic <meronic@naver.com>
2021-05-29 23:13:11 +0900

    login test

--- main/views.py
+++ main/views.py
@@ -1,7 +1,12 @@
+from django import template
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request) : 
+    template = loader.get_template('mainapp/index.html')
+    context = {
+        'lastest_question_list' : "test",
+    }
     
-    return HttpResponse("hello index.html")
+    return HttpResponse(template.render(context, request))
